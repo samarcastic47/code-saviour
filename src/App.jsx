a0c1786e0125af30812e281aad4f045bf33ecb71
samarcastic47 <120738736+samarcastic47@users.noreply.github.com>
2024-05-19 12:40:12 +0530

    Update App.jsx
    added Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import { Analytics } from "@vercel/analytics/react"
 import Home from "./Home";
 import Error from './Error';
 import { ThemeProvider } from '@mui/material/styles';
